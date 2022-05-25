6ae0388a7f44e532c2e19d2f069d7eb87e3c450a
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-05-25 21:49:40 +0200

    Singleton code example

---
+++ Patterns/Singleton/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <list>
+
+class Database
+{
+private:
+    static Database *instance;
+
+    std::list<int> data;
+
+    Database() {}
+
+public:
+    static Database *getDatabase();
+
+    // ------- business logic -------
+    void addData(int data)
+    {
+        this->data.push_back(data);
+    }
+
+    void traverseDatabaseData()
+    {
+        for (auto i = this->data.begin(); i != data.end(); i++)
+        {
+            std::cout << (*i) << ' ';
+        }
+        std::cout << '\n';
+    }
+};
+
+Database *Database::instance = nullptr;
+
+Database *Database::getDatabase()
+{
+    if (instance == nullptr)
+    {
+        instance = new Database();
+        std::cout << "Now you are connected to a database!\n";
+    }
+    return instance;
+}
+
+int main()
+{
+    Database *db_first_inst = Database::getDatabase();
+
+    db_first_inst->addData(10);
+    db_first_inst->addData(20);
+    db_first_inst->addData(30);
+    db_first_inst->addData(40);
+
+    db_first_inst->traverseDatabaseData();
+
+    Database *db_second_inst = Database::getDatabase();
+
+    db_second_inst->addData(100);
+
+    db_first_inst->traverseDatabaseData();
+
+    if (db_first_inst == db_second_inst)
+        std::cout << "Singleton works, both variables contain the same instance.\n";
+
+    delete db_first_inst;
+
+    return 0;
+}
