bfbb500d61c01adb7a3d04f5793b1800b93f409c
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-05-29 16:31:16 +0200

    to upper

--- Patterns/Singleton/main.cpp
+++ Patterns/Singleton/main.cpp
@@ -15,15 +15,15 @@ public:
 
     void operator=(const Database &) = delete; // singletons should not be assignable.
 
-    static Database *getDatabase();
+    static Database *GetDatabase();
 
     // ------- business logic -------
-    void addData(int data)
+    void AddData(int data)
     {
         this->data.push_back(data);
     }
 
-    void traverseDatabaseData()
+    void TraverseDatabaseData()
     {
         for (auto i = this->data.begin(); i != data.end(); i++)
         {
@@ -35,7 +35,7 @@ public:
 
 Database *Database::instance = nullptr;
 
-Database *Database::getDatabase()
+Database *Database::GetDatabase()
 {
     if (instance == nullptr)
     {
@@ -47,20 +47,20 @@ Database *Database::getDatabase()
 
 int main()
 {
-    Database *db_first_inst = Database::getDatabase();
+    Database *db_first_inst = Database::GetDatabase();
 
-    db_first_inst->addData(10);
-    db_first_inst->addData(20);
-    db_first_inst->addData(30);
-    db_first_inst->addData(40);
+    db_first_inst->AddData(10);
+    db_first_inst->AddData(20);
+    db_first_inst->AddData(30);
+    db_first_inst->AddData(40);
 
-    db_first_inst->traverseDatabaseData();
+    db_first_inst->TraverseDatabaseData();
 
-    Database *db_second_inst = Database::getDatabase();
+    Database *db_second_inst = Database::GetDatabase();
 
-    db_second_inst->addData(100);
+    db_second_inst->AddData(100);
 
-    db_first_inst->traverseDatabaseData();
+    db_first_inst->TraverseDatabaseData();
 
     if (db_first_inst == db_second_inst)
         std::cout << "Singleton works, both variables contain the same instance.\n";
